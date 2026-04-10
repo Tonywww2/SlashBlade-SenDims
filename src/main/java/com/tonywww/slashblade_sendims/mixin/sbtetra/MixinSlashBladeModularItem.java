26d3c5b52b78068cd13ce12e9f848fbb6720c866
Tonywww <1204484415@qq.com>
2026-04-09 22:35:18 -0700

    SBT name

---
+++ src/main/java/com/tonywww/slashblade_sendims/mixin/sbtetra/MixinSlashBladeModularItem.java
@@ -0,0 +1,26 @@
+package com.tonywww.slashblade_sendims.mixin.sbtetra;
+
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
+import net.yiran.sbtetra.item.ISlashBladeTetra;
+import net.yiran.sbtetra.item.SlashBladeModularItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SlashBladeModularItem.class)
+public abstract class MixinSlashBladeModularItem extends ItemSlashBlade implements ISlashBladeTetra {
+
+    public MixinSlashBladeModularItem(Tier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
+        super(tier, attackDamageIn, attackSpeedIn, builder);
+    }
+
+    @Inject(method = "getName(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/network/chat/Component;", at = @At("HEAD"), cancellable = true, remap = false)
+    private void slashblade_sendims$onGetName(ItemStack stack, CallbackInfoReturnable<Component> cir) {
+        String id = this.getDescriptionId(stack);
+        cir.setReturnValue(Component.literal(this.getDisplayNamePrefixes(stack)).append(Component.translatable(id)));
+    }
+}
