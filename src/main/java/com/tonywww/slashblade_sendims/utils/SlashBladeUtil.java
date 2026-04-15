a248df667eba16f4f8c2c1039bedf1637982b12e
Tonywww <1204484415@qq.com>
2026-04-15 02:43:07 -0700

    拔刀剑utils

--- src/main/java/com/tonywww/slashblade_sendims/utils/SlashBladeUtil.java
+++ src/main/java/com/tonywww/slashblade_sendims/utils/SlashBladeUtil.java
@@ -2,6 +2,10 @@ package com.tonywww.slashblade_sendims.utils;
 
 import mods.flammpfeil.slashblade.capability.slashblade.ISlashBladeState;
 import mods.flammpfeil.slashblade.item.ItemSlashBlade;
+import mods.flammpfeil.slashblade.registry.slashblade.SlashBladeDefinition;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
 
@@ -14,6 +18,13 @@ public class SlashBladeUtil {
 
     public static ISlashBladeState getState(ItemStack stack) {
         return stack.getCapability(state).resolve().orElseThrow(() -> new IllegalStateException("ItemStack 缺失 ISlashBladeState 能力"));
+    }
+
+    public static ItemStack getBladeItemStack(RegistryAccess registryAccess, ResourceLocation bladeKey) {
+
+        return registryAccess.registryOrThrow(SlashBladeDefinition.REGISTRY_KEY)
+                .getOrThrow(ResourceKey.create(SlashBladeDefinition.REGISTRY_KEY, bladeKey))
+                .getBlade();
     }
 
 }
