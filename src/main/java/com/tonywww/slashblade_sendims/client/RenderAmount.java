e10fdfd0ff23fa06f51b000a36a61b76f9503db4
yiran1457 <3270897489@qq.com>
2026-06-05 21:50:14 +0800

    修改文本存活时间

--- src/main/java/com/tonywww/slashblade_sendims/client/RenderAmount.java
+++ src/main/java/com/tonywww/slashblade_sendims/client/RenderAmount.java
@@ -1,10 +1,11 @@
 package com.tonywww.slashblade_sendims.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class RenderAmount {
-    public int life = 80;
+    public float life = 80;
     public int x;
     public int y;
     public String amount;
@@ -18,12 +19,12 @@ public class RenderAmount {
     public boolean render(GuiGraphics guiGraphics, Font font) {
         int alpha;
         if (life < 40) {
-            alpha = (int) (life / 40.0f * 255);
+            alpha = (int) (life / 40 * 255);
         } else {
             alpha = 255;
         }
         int color = (alpha << 24) | 0x00FF0000;
         guiGraphics.drawString(font, amount, x - font.width(amount), y, color);
-        return --life < 1;
+        return (life -= Minecraft.getInstance().getPartialTick()) <= 0.05;
     }
 }
